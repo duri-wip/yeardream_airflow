47a6abe9557a24e174f6c4fd19b972b6110d94aa
sw <8s.eow.ooc@gmail.com>
2024-06-17 09:27:52 +0900

    standard 수정

--- dags/dags_bash_operator_standard.py
+++ dags/dags_bash_operator_standard.py
@@ -1,25 +1,27 @@
+
+
 from airflow import DAG
 import pendulum
-
-import datetime
-
 from airflow.operators.bash import BashOperator
 
-### 스케줄: 매주 월요일, 금요일 09:00 - start_date: 2024-06-01 - tag: homework
-
-my_dag = DAG(
-     dag_id="dags_bash_operator_standard",
-     start_date=datetime.datetime(2024, 6, 1),
-     tag = "homework",
-     schedule="0 9 * * 1,5"
+dag = DAG(
+    dag_id="dags_bash_operator_standard",
+    schedule="0 13 * * 5#2",
+    start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=['homework']
 )
 
-bash_t1 = BashOperator(task_id="bash_task1",
-            bash_command="echo whoami",
-            dag=my_dag)
+bash_t1 = BashOperator(
+    task_id="bash_t1",
+    bash_command="echo whoami",
+    dag=dag
+)
 
-bash_t2 = BashOperator(task_id="bash_task2",
-              bash_command="echo $HOSTNAME",
-              dag=my_dag)
+bash_t2 = BashOperator(
+    task_id="bash_t2",
+    bash_command="echo $HOSTNAME",
+    dag=dag
+)
 
-bash_task1 >> bash_task2
+bash_t1 >> bash_t2
